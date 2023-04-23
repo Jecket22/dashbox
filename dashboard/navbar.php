2b0a0314a2b1354c721672019c77814430e24041
KontrollFreek <williambush357@gmail.com>
2023-04-23 10:55:03 -0400

    removed server status

--- dashboard/navbar.php
+++ dashboard/navbar.php
@@ -6,12 +6,12 @@ session_start();
 <nav>
   <span>
     <a href="/dashboard/">Home</a>
-    <a href="javascript:void(0)">Server Status</a>
     <span id="statistics">
       <button>
         <p>Statistics</p>
-        <p data-button="on">▼</p>
-        <p data-button="off">▶</p>
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+        </svg>
       </button>
       <div>
         <a href="javascript:void(0)">Levels</a>
@@ -24,8 +24,9 @@ session_start();
     <span id="tools">
       <button>
         <p>Tools</p>
-        <p data-button="on">▼</p>
-        <p data-button="off">▶</p>
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+        </svg>
       </button>
       <div>
         <a href="javascript:void(0)">Account Management</a>
@@ -42,7 +43,7 @@ session_start();
   <span>
     <?php // Logged In?
     if (empty($_SESSION['password']))
-      echo '<a href="/dashboard/login/">Login</a><a href="/dashboard/register/">Register</a>';
+      echo '<a href="/dashboard/login/">Log In</a><a href="/dashboard/register/">Sign Up</a>';
     else {
       $requireVerified = strval((Config::GetVariable("accounts", "verifyLevel") != 0) ? 1 : 0);
 
@@ -50,7 +51,7 @@ session_start();
       $login->execute(array(':requireVerified' => $requireVerified, ':password' => $_SESSION['password']));
       $password = $login->fetchColumn();
       if (empty($password))
-        echo '<a href="/dashboard/login/">Login</a><a href="/dashboard/register/">Register</a>';
+        echo '<a href="/dashboard/login/">Log In</a><a href="/dashboard/register/">Sign Up</a>';
       else {
         echo '<a href="/dashboard/profile/">Profile</a>';
         $_SESSION['password'] = $password;
