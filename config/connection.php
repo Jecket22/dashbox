cc703956350b3f60fbba39bfef1dea9a9af01524
Jecket22 <lez.vezzica@gmail.com>
2023-04-29 16:05:42 +0200

    Hide credentials by using php instead of json holy shit I'm fucking stupid + change sql file to support mysql and no longer use DROP IF EXIST

---
+++ config/connection.php
@@ -0,0 +1,7 @@
+<?php
+$server = "localhost"; // This is the domain/ip address of the mysql/mariadb server.
+$port = 3306; // Port of the server, usually 3306
+$user = "superuser";
+$password = "s4mpel_p@55w0rd";
+$database = "dashbox";
+?>
