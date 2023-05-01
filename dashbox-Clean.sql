d4cb7a16c3fe737d025c4754e0eef356a18e934c
KontrollFreek <williambush357@gmail.com>
2023-05-01 11:10:31 -0400

    userdata table

--- dashbox-Clean.sql
+++ dashbox-Clean.sql
@@ -72,15 +72,52 @@ INSERT INTO `quests` (`id`, `name`, `requirement`, `reward`, `type`) VALUES
 
 CREATE TABLE IF NOT EXISTS `userdata` (
   `id` int(11) NOT NULL,
+  
   `stars` int(11) NOT NULL DEFAULT 0,
-  `demons` int(11) NOT NULL DEFAULT 0,
+  `diamonds` int(11) NOT NULL DEFAULT 0,
   `starCoins` int(11) NOT NULL DEFAULT 0,
   `userCoins` int(11) NOT NULL DEFAULT 0,
-  `diamonds` int(11) NOT NULL DEFAULT 0,
-  `iconType` int(11) NOT NULL DEFAULT 0,
-  `iconArray` varchar(255) NOT NULL DEFAULT '0,0,0,0,0,0,0,0,0,0,0',
+  `demons` int(11) NOT NULL DEFAULT 0,
+  `creatorPoints` int(11) NOT NULL DEFAULT 0,
+
+  `orbs` int(11) NOT NULL DEFAULT 0,
+  `completedLevels` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `IP` varchar(45) NOT NULL DEFAULT '127.0.0.1',
+  `friends` int(11) NOT NULL DEFAULT 0,
+
+  `mainIcon` int(11) NOT NULL DEFAULT 0 COMMENT 'Determines the icon used for displaying in comments, leaderboard, etc.',
+  `player` int(11) NOT NULL DEFAULT 0 COMMENT '0',
+  `ship` int(11) NOT NULL DEFAULT 0 COMMENT '1',
+  `ball` int(11) NOT NULL DEFAULT 0 COMMENT '2',
+  `ufo` int(11) NOT NULL DEFAULT 0 COMMENT '3',
+  `wave` int(11) NOT NULL DEFAULT 0 COMMENT '4',
+  `robot` int(11) NOT NULL DEFAULT 0 COMMENT '5',
+  `spider` int(11) NOT NULL DEFAULT 0 COMMENT '6',
+  `glow` int(11) NOT NULL DEFAULT 0,
+  `explosion` int(11) NOT NULL DEFAULT 0,
+  `trail` int(11) NOT NULL DEFAULT 0,
+  `colour1` int(11) NOT NULL DEFAULT 0,
+  `colour2` int(11) NOT NULL DEFAULT 0,
+
+  `messages` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 - All, 1 - Only Friends, 2 - Nobody',
+  `friendRequests` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 - All, 2 - Nobody',
+  `comments` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 - All, 1 - Only Friends, 2 - Nobody',
+
+  `youtube` varchar(255) NOT NULL DEFAULT '',
+  `twitter` varchar(255) NOT NULL DEFAULT '',
+  `twitch` varchar(255) NOT NULL DEFAULT '',
+
+  `creation` int(11) NOT NULL DEFAULT 0 COMMENT 'Account creation date',
+  `lastLogOn` int(11) NOT NULL DEFAULT 0,
+
   `leaderboardBanned` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Not checked if acc is disabled',
   `creatorBanned` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'leaderboardBanned will affect this',
+
+  `chest1` int(11) NOT NULL DEFAULT 0 COMMENT 'Timestamp',
+  `chest2` int(11) NOT NULL DEFAULT 0 COMMENT 'Timestamp',
+  `chestCount1` int(11) NOT NULL DEFAULT 0 COMMENT 'In-game chest ID',
+  `chestCount2` int(11) NOT NULL DEFAULT 0 COMMENT 'In-game chest ID',
+
   PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `leaderboardBanned` (`leaderboardBanned`),
@@ -88,8 +125,8 @@ CREATE TABLE IF NOT EXISTS `userdata` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='This is not connected to the cloud save data';
 
 CREATE TABLE IF NOT EXISTS `dailyquests` (
-  `id` int(11) NOT NULL COMMENT 'The order of the quests',
-  `questID` int(11) NOT NULL COMMENT 'The ID of this specific quest',
+  `id` int(11) NOT NULL,
+  `questID` int(11) NOT NULL,
   `expire` timestamp NOT NULL DEFAULT NOW() COMMENT "The timestamp that the quest will expire. This should theoretically be the same for all quests, I just don't know where else to store it",
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Only used for daily quests, otherwise this is unused';
